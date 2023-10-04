4cb026f741cb896755600ba088a79662ebbfa84c
rpnz <ritesh@itguru.co.nz>
2023-10-04 20:05:19 +1300

    First update

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function ()
+    require('neo-tree').setup {
+      close_if_last_window = false,
+      popup_border_style = "rounded"
+    }
+  end,
+}
