e767d3adb6f037b69c779dd3724b098ad22e1abc
rpnz <ritesh@itguru.co.nz>
2023-10-07 21:01:04 +1300

    Added bufferline

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -9,7 +9,14 @@ return {
   config = function ()
     require('neo-tree').setup {
       close_if_last_window = false,
-      popup_border_style = "rounded"
+      popup_border_style = "rounded",
+      follow_current_file = true,
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
     }
   end,
 }
--- lua/customconfig/customkeymaps.lua
+++ lua/customconfig/customkeymaps.lua
@@ -1,1 +1,2 @@
 vim.keymap.set("n", "<leader>e", ":Neotree<CR>", { desc = '[e]Open Neotree'})
+vim.keymap.set("n", "<leader>bu", ":BufferLinePick<CR>", { desc = '[bu]Pick Buffer'})
